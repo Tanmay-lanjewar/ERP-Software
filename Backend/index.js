dc67b939c7017ba526745920e6249707bc36708c
Tanmay-lanjewar <tanmaylanjewar11@gmail.com>
2025-06-25 03:17:51 +0530

    mysql2 connected

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+const db = require('./config/db');
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req,res)=>{
-    res.send('Hello, Worlds!');
-})
+app.get('/', (req, res) => {
+  db.query('SELECT 1', (err, results) => {
+    if (err) return res.status(500).send('DB Connection Failed');
+    res.send('✅ MySQL Connected Successfully');
+  });
+});
 
-
-
-let port = 3000;
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
